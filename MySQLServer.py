9283a2e0695512e97847cc176d648a9cb840c869
mohamedshmela <mohamedshmela339@gmail.com>
2024-07-28 19:52:16 +0300

    first try to make the second task

---
+++ MySQLServer.py
@@ -0,0 +1,13 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    password = "12345shmela@sql"
+)
+
+mycursor = mydb.cursor()
+mycursor.execute("CREATE DATABASE IF NOT EXIST alx_book_store")
+
+mycursor.close()
+mydb.close()
