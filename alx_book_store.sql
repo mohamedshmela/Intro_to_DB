631c17cc6483fae8468b7e783fdbe77715ad5096
mohamedshmela <mohamedshmela339@gmail.com>
2024-07-28 19:42:37 +0300

    finishing the first task

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -7,3 +7,24 @@ author_id VARCHAR(130),
 price DOUBLE,
 publication_date DATE
 );
+CREATE TABLE Authors (
+    author_id INT PRIMARY Key,
+    author_name VARCHAR(215)
+);
+CREATE Table Customers (
+    customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(215),
+    email VARCHAR(215),
+    address TEXT
+);
+CREATE TABLE Orders (
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE
+);
+CREATE TABLE Order_Details (
+    order_details_id INT PRIMARY KEY,
+    order_id INT,
+    book_id INT,
+    quantity DOUBLE
+);
