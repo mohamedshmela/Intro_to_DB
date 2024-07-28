0d4fd5c55c7feb820dd079349784c9db63c2ec13
mohamedshmela <mohamedshmela339@gmail.com>
2024-07-28 20:01:17 +0300

    new change to the second task

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,13 +1,19 @@
 import mysql.connector
 
-mydb = mysql.connector.connect(
-    host = "localhost",
-    user = "root",
-    password = "12345shmela@sql"
-)
+try:
+    mydb = mysql.connector.connect(
+        host = "localhost",
+        user = "root",
+        password = "12345shmela@sql"
+    )
+    if mydb.is_connected():
+        mycursor = mydb.cursor()
+        mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+        print("Database 'alx_book_store' created successfully!")
+except mysql.connector.Error as e:
+    print(f"an error occured: {e}")
 
-mycursor = mydb.cursor()
-mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
-
-mycursor.close()
-mydb.close()
+finally:
+    mycursor.close()
+    mydb.close()
+    print("my sql connection is closed")
