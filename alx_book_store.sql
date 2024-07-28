afd0f7c5bf9664f8fe396581787c1c7d0fab66cb
mohamedshmela <mohamedshmela339@gmail.com>
2024-07-28 19:46:32 +0300

    new change to the first task

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -19,12 +19,12 @@ CREATE Table Customers (
 );
 CREATE TABLE Orders (
     order_id INT PRIMARY KEY,
-    customer_id INT,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
     order_date DATE
 );
 CREATE TABLE Order_Details (
     order_details_id INT PRIMARY KEY,
-    order_id INT,
-    book_id INT,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id),
     quantity DOUBLE
 );
