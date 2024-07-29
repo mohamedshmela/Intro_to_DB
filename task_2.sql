dc4288888651e7e6ec30f5941984432bc7594b02
mohamedshmela <mohamedshmela339@gmail.com>
2024-07-29 06:41:46 +0300

    the third task

---
+++ task_2.sql
@@ -0,0 +1,30 @@
+CREATE DATABASE IF NOT EXISTS alx_book_store
+USE alx_book_store;
+CREATE TABLE Books (
+book_id INT PRIMARY KEY,
+title VARCHAR(130),
+author_id VARCHAR(130),
+price DOUBLE,
+publication_date DATE
+);
+CREATE TABLE Authors (
+    author_id INT PRIMARY Key,
+    author_name VARCHAR(215)
+);
+CREATE Table Customers (
+    customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(215),
+    email VARCHAR(215),
+    address TEXT
+);
+CREATE TABLE Orders (
+    order_id INT PRIMARY KEY,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
+    order_date DATE
+);
+CREATE TABLE Order_Details (
+    order_details_id INT PRIMARY KEY,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id),
+    quantity DOUBLE
+);
